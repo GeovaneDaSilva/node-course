6e23161e90fd7cb4117ba23ba72188dec5f7adac
gdasilvarocketcyber <gdasilva@rocketcyber>
2021-09-16 17:26:06 -0400

    ci: add cors middlewares access allow

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { bodyParser } from './../middlewares/body-parser';
 import { Express } from 'express'
+import { cors } from '../middlewares/cors';
 
 export default (app: Express): void => {
     app.use(bodyParser)
+    app.use(cors)
 }
