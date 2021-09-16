74cbb82490b8b1d9a33ae5e5aa1ebf6dc35af2b8
gdasilvarocketcyber <gdasilva@rocketcyber>
2021-09-16 17:31:15 -0400

    refactor: ci cors middlewares access allow more changes

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,5 @@
-import { bodyParser } from './../middlewares/body-parser';
 import { Express } from 'express'
-import { cors } from '../middlewares/cors';
+import { bodyParser, cors } from '../middlewares/index'
 
 export default (app: Express): void => {
     app.use(bodyParser)
