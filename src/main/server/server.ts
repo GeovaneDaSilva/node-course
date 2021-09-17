1cc69c46728e0eb89b3a1e3eb00b97f89b06992d
gdasilvarocketcyber <gdasilva@rocketcyber>
2021-09-16 23:06:19 -0400

    feat: connect mongoose and created adapter helper

--- src/main/server/server.ts
+++ src/main/server/server.ts
@@ -1,4 +1,16 @@
-import app from "../config/app";
+import { MongooseHelper } from "../../infra/db/mongodb/mongoose-helper/mongoose-helper";
+import { config as dotenv } from 'dotenv'
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`'Express server port: \x1b[32m${process.env.PORT}\x1b[0m`)})
+
+dotenv()
+MongooseHelper.connect(process.env.MONGODB)
+.then(async () => {
+    const app = (await import('../../main/config/app')).default
+
+    app.listen(process.env.PORT, ()=>{
+        console.log(`'Express server port: \x1b[32m${process.env.PORT}\x1b[0m`)})
+        console.log('Base de datos  puerto 27017: \x1b[32m%s\x1b[0m', 'online')
+
+})
+
+
