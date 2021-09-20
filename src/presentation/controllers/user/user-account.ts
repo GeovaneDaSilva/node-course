5fe9d8a7de28a0fd7025cbd36631497c9e183479
gdasilvarocketcyber <gdasilva@rocketcyber>
2021-09-20 12:08:56 -0400

    feat: usecase add account and message error

--- src/presentation/controllers/user/user-account.ts
+++ src/presentation/controllers/user/user-account.ts
@@ -1,8 +1,24 @@
 import { HttpRequest, HttpResponse } from './../../interfaces/protocols/http'
 import { Controller } from './../../interfaces/protocols/controller'
+import { IAddAccount } from '../../../domain/useCases/account/add-user-account'
+import { serverError, success } from '../../interfaces/protocols/http-response'
 
-export class RegisterUserAccount implements Controller{
+export class RegisterUserAccount implements Controller {
+    constructor(private readonly iAddAccount: IAddAccount) {
+        this.iAddAccount = iAddAccount
+    }
     async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-        return
+        try {
+            const { name, email, password, created_at, activated_at, role } = httpRequest.body
+
+            const bodyRequest = await this.iAddAccount.add({
+                name, email, password, created_at, activated_at, role
+            })
+
+            return success(bodyRequest)
+
+        } catch (error) {
+            return serverError(error)
+        }
     }
 }
