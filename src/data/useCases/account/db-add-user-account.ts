c8db097331371e07c8aa75d411df55203e96741b
gdasilvarocketcyber <gdasilva@rocketcyber>
2021-09-28 13:55:51 -0400

    chore: implements user repository

--- src/data/useCases/account/db-add-user-account.ts
+++ src/data/useCases/account/db-add-user-account.ts
@@ -1,11 +1,13 @@
 import { UserAccount } from "../../../domain/entities/user";
 import { IAddAccount } from "../../../domain/useCases/account/add-user-account";
+import { IUserRepository } from "../../contract-repository/user-repository";
 
 export class DbAddAccount implements IAddAccount{
+    constructor (private readonly iUserRepository: IUserRepository) {
+        this.iUserRepository = iUserRepository
+    }
     async add (account: UserAccount): Promise<UserAccount> {
-
-        console.log(account);
-        
-        return account
+        const userDb = await this.iUserRepository.add(account)
+        return userDb
     }
 }
