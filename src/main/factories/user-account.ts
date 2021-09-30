e67e2e76539142b59a0f9763bd831494d34b4683
gdasilvarocketcyber <gdasilva@rocketcyber>
2021-09-30 12:45:24 -0400

    ci: implement encrypt in db add account

--- src/main/factories/user-account.ts
+++ src/main/factories/user-account.ts
@@ -1,11 +1,14 @@
 import { DbAddAccount } from "../../data/useCases/account/db-add-user-account"
 import { UserMongoRepository } from "../../infra/db/mongodb/repository/user-repository"
 import { RegisterUserAccountController } from "../../presentation/controllers/user/user-account"
+import { BcryptAdapter } from "../../utils-adapters/bcrypt-adapter"
 import { EmailValidatorAdapter } from "../../utils-adapters/email-validator-adapter"
 
 export const makeRegisterUserAccountController = (): RegisterUserAccountController => {
+    const salt = 10
     const userMongoRepository = new UserMongoRepository()
-    const dbAddAccount = new DbAddAccount(userMongoRepository)
+    const bcryptAdapter = new BcryptAdapter(salt)
+    const dbAddAccount = new DbAddAccount(userMongoRepository, bcryptAdapter)
     const emailValidatorAdapter = new EmailValidatorAdapter()
     const registerUserAccount = new RegisterUserAccountController(dbAddAccount, emailValidatorAdapter, userMongoRepository)
     return registerUserAccount
