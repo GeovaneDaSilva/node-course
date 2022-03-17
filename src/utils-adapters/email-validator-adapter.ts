5beeb6b16beae2cefc44b6f0f2356c17197a0204
inmigrausa <96200886+inmigrausa@users.noreply.github.com>
2022-03-16 21:33:12 -0400

    chore: replace email validator

--- src/utils-adapters/email-validator-adapter.ts
+++ src/utils-adapters/email-validator-adapter.ts
@@ -1,8 +1,8 @@
-import validator from 'email-validator'
+import * as EmailValidator from 'node-email-validation';
 import { IEmailValidator } from "../services/email-validator-adapter";
 
 export class EmailValidatorAdapter implements IEmailValidator {
     isValid (email: string): boolean {
-        return validator.validate(email)
+        return EmailValidator.is_email_valid(email)
     }
 }
